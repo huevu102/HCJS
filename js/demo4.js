d358051b2bd18653a76ad5f7cd0f69bd23f78bdd
HueVu <huevt.jp@gmail.com>
2022-09-09 19:39:40 +0700

    commit code

--- js/demo4.js
+++ js/demo4.js
@@ -1,4 +1,6 @@
-function update(){
+function update(city){
+    var bg = document.getElementById("bg-fade");
+    bg.style.display = "block"; // Hiện ảnh gif loading... trong khi chờ tải dữ liệu
     var xhttp = new XMLHttpRequest();
     xhttp.onload = function (){
         // console.log(this.responseText);
@@ -11,8 +13,9 @@ function update(){
 
         document.getElementById("city").innerText = data.name + ", " + data.sys.country;
         document.getElementById("press").innerText = data.main.pressure;
+        bg.style.display = "none"; //Ẩn ảnh gif loading khi đã lấy đc dữ liệu
     };
-    var link = "https://api.openweathermap.org/data/2.5/weather?q=new york&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    var link = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
     xhttp.open("get", link, true); //async: chạy theo phương thức đồng bộ hoá
     xhttp.send();
 }
---
+++ js/js-asm-3.js
@@ -0,0 +1,13 @@
+function update(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = function (){
+        var data = JSON.parse(this.responseText);
+
+        document.getElementById("city").innerText = data.name + ", " + data.sys.country;
+        document.getElementById("temp").innerText = data.main.temp;
+        document.getElementById("pres").innerText = data.main.pressure;
+    };
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=Hanoi,vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric";
+    xhttp.open("get", url, true);
+    xhttp.send();
+}
